7d14371fce52452aff21ece7e683fefb240268a3
unknown <emilabdurahmanli05@gmail.com>
2023-08-14 18:38:36 +0400

    first commit

---
+++ app/src/main/java/com/emilabdurahmanli/einentodoapp/ui/activiry/HomeActivity.kt
@@ -0,0 +1,12 @@
+package com.emilabdurahmanli.einentodoapp.ui.activiry
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.emilabdurahmanli.einentodoapp.R
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dashboard)
+    }
+}
